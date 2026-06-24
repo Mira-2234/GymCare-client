4202dfcce45fe12d1fd83e7c1ec3e36d0ecc32c3
Mira-2234 <tasmimmira822@gmail.com>
2026-06-24 10:26:41 +0600

    fix: allow external image domains

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,12 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "loremflickr.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ibb.co",
-      },
-      {
-        protocol: "https",
-        hostname: "api.dicebear.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
+      { protocol: "https", hostname: "loremflickr.com" },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "**.ibb.co" },
+      { protocol: "https", hostname: "api.dicebear.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
